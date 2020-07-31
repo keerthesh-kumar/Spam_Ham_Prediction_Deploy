a18538c5db5cdfcae83d825d917501285fdd8b79
Keerthesh Kumar K S <50326810+keerthesh-kumar@users.noreply.github.com>
2020-07-31 17:23:55 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask,render_template,url_for,request
+import pickle
+
+# load the model from disk
+filename = 'nlp_model.pkl'
+clf = pickle.load(open(filename, 'rb'))
+cv = pickle.load(open('tranform.pkl','rb'))
+
+#Local Host link to app.py
+# http://127.0.0.1:5000/
+
+# Indicates the main in python
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+	if request.method == 'POST':
+		message = request.form['message']
+		data = [message]
+		vect = cv.transform(data).toarray()
+		my_prediction = clf.predict(vect)
+	return render_template('result.html',prediction = my_prediction)
+
+if __name__ == '__main__':
+	app.run(debug=True)
